37284d5307e34b0790431e72634d206aaa4c7841
OctaVianu8 <stanescu.matei.octavian@gmail.com>
2024-12-07 00:49:53 +0200

    finish temoporary login-page

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <StrictMode>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+   </StrictMode>
+);
